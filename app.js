d06f8408fdd7fbea1a74bb18a528099b05a0d712
Chetan Rathod <200050116@iitb.ac.in>
2022-07-04 23:45:18 +0530

    todoList Webapp complete

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-chetan:test@cluster0.wavljc5.mongodb.net/todolistDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
 
 const itemsSchema = {
   name: String
